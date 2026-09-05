343f30032ad13e3e9685297f75ca5145028aa3b4
Tarif Hassan <tarifhassan27@gmail.com>
2026-09-05 11:27:50 +0600

    Add and load raw tables (Phase 2)

--- sql/00_setup/01_create_schemas.sql
+++ sql/00_setup/01_create_schemas.sql
@@ -1,2 +1,34 @@
 CREATE SCHEMA IF NOT EXISTS raw;
 CREATE SCHEMA IF NOT EXISTS clean;
+CREATE TABLE raw.emp_sex_occupation (
+    ref_area        VARCHAR,
+    source          VARCHAR,
+    indicator       VARCHAR,
+    sex             VARCHAR,
+    classif1        VARCHAR,
+    time            VARCHAR,
+    obs_value       VARCHAR,
+    obs_status      VARCHAR,
+    note_classif    VARCHAR,
+    note_indicator  VARCHAR,
+    note_source     VARCHAR
+);
+
+CREATE TABLE raw.sdg_women_managers (
+    ref_area        VARCHAR,
+    source          VARCHAR,
+    indicator       VARCHAR,
+    time            VARCHAR,
+    obs_value       VARCHAR,
+    obs_status      VARCHAR,
+    note_indicator  VARCHAR,
+    note_source     VARCHAR
+);
+
+CREATE TABLE raw.wb_country_class (
+    economy           VARCHAR,
+    code              VARCHAR,
+    region            VARCHAR,
+    income_group      VARCHAR,
+    lending_category  VARCHAR
+)
